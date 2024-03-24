4fa1cb2a1d597dd85249fa58fa53a8f18e6c5552
Your Name <you@example.com>
2024-03-24 18:18:52 +0000

    form added using react form hook needs stlying etc will need connecting to back end when this ready

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+// Assuming this will link to back end when this is in place needs sytling
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <>
-      <h2>Contact us</h2>
-      <div className="contactFormContainer">
-        <></>
-      </div>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <select {...register("Title", { required: true })}>
+        <option value="Mr">Mr</option>
+        <option value="Mrs">Mrs</option>
+        <option value="Miss">Miss</option>
+        <option value="Dr">Dr</option>
+      </select>
+      <input
+        type="text"
+        placeholder="First name"
+        {...register("First name", { required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="Last name"
+        {...register("Last name", { required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <textarea {...register("Message", { required: true })} />
+
+      <input type="submit" />
+    </form>
   );
 };
 
